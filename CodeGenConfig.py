d5fda9b932788d715ed567fed65d11fc188b41a0
DeepakElango <deepakelango05@gmail.com>
2025-12-28 11:29:08 +0000

    Initial commit: first version

---
+++ CodeGenConfig.py
@@ -0,0 +1,9 @@
+class CodeGeneratorConfig:
+    file_name:str="module_with_param"
+    required_headers:list[str]=["init","module","moduleparam","printk","kernal"]
+    file_design:str="mod"
+    init_function_name:str="start_func"
+    cleanup_function_name:str="exit_func"
+    author_name:str="Deepak"
+    file_description:str="Example of module with parameter"
+    License:str="GPL"
